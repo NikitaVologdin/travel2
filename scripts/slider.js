8e1fa85ed6e5cb1121448378cbcc1da273a27abb
Nikita Vologdin <nvologdins@gmail.com>
2021-09-24 12:02:03 +0300

    slider with no animation finished

--- scripts/slider.js
+++ scripts/slider.js
@@ -19,68 +19,70 @@ const feedbacks = [
     }
 ]
 
-function addSlides() {
-    for (let i = 0; i < feedbacks.length; i++) {
-        if ( i === 0) {
-            slider.insertAdjacentHTML('afterbegin', addSliderItem(i, true))
-        } else {
-            slider.insertAdjacentHTML('afterbegin', addSliderItem(i))
-        } 
-    }
-}
-
-function addSliderItem (index, active = false) {
-    if (active === true) {
-        return `
-        <div class="slider__item slider-item _active" id="slide${index}">
-            <img src="images/main/MikeTaylor@2x.png" alt="Feedback authors picture" class="slider-item__authorImg" id="authorImg">
-            <p class="slider-item__content">“${feedbacks[index].content}”</p>
-            <h5 class="slider-item__authorName">${feedbacks[index].name}</h5>
-            <h5 class="slider-item__authorLocation">${feedbacks[index].location}</h5>
-        </div>`
-    } else {
+function addSliderItem (index) {
         return `
         <div class="slider__item slider-item " id="slide${index}">
             <img src="images/main/MikeTaylor@2x.png" alt="Feedback authors picture" class="slider-item__authorImg" id="authorImg">
-            <p class="slider-item__content">“${feedbacks[index].content}”</p>
+            <p class="slider-item__content">${feedbacks[index].content}</p>
             <h5 class="slider-item__authorName">${feedbacks[index].name}</h5>
             <h5 class="slider-item__authorLocation">${feedbacks[index].location}</h5>
         </div>`
-    }
-
 }
 
-addSlides()
+function addSliderPreviousItem (index) {
+    return `<div class="slider-item__previous-item"><div class="previous-item">
+    <h5 class="slider-item__authorName">${feedbacks[index].name}</h5>
+    <h5 class="slider-item__authorLocation">${feedbacks[index].location}</h5></div>
+</div>`
+}
 
-// let sliderItem = createElement('div')
-// sliderItem.classList.add('slider__item', 'slider-item')
-// sliderItem.id = `slide${index}`
-// let sliderItemAuthorImg = createElement('div')
-// let sliderItemParagraph = createElement('p')
-// let authorName = createElement('h5')
-// let authorLocation = createElement('h5')
-// slider.appendChild(sliderItem)
+function makeActiveSlide (cssSelector) {
+    let firstSlide = document.querySelectorAll(cssSelector)
 
+    if (cssSelector === ".slider-item__previous-item") {
+        firstSlide[1].classList.add('_active')
+    } else if ( cssSelector === ".slider-item") {
+        firstSlide[0].classList.add('_active')
+    }
+}
+
+function addSlides(addItem) {
+    for (let i = 0; i < feedbacks.length; i++) {
+        slider.insertAdjacentHTML('afterbegin', addItem(i))
+    }
+}        
+
+function fillSlider() {
+    addSlides(addSliderItem)
+    addSlides(addSliderPreviousItem)
+    makeActiveSlide(".slider-item")
+    makeActiveSlide(".slider-item__previous-item")
+}
+
+fillSlider()
 
 const slides = document.querySelectorAll('.slider-item')
+const previousSlides = document.querySelectorAll('.slider-item__previous-item')
 const buttons = document.querySelector('.switch')
-let firstSlide = document.querySelector('#slide0')
-firstSlide.classList.add('_active')
 
 buttons.addEventListener('click', (event)=>{
 
-    let activeSlide = slider.querySelector('._active')
+    let activeSlide = slider.querySelector('.slider-item._active')
+    let activePreviousSlide = slider.querySelector('.slider-item__previous-item._active')
     let index = Number(event.target.getAttribute('data-index'))
     let activeButton = buttons.querySelector('.switch__button_active')
 
     if(event.target.id === 'button0') {
         displaySlide(index, activeSlide)
+        displayNextPreviousSlide(index, activePreviousSlide)
         displayActiveButton(activeButton, event)
     } else if (event.target.id === 'button1') {
         displaySlide(index, activeSlide)
+        displayNextPreviousSlide(index, activePreviousSlide)
         displayActiveButton(activeButton, event)
     } else if (event.target.id === 'button2') {
         displaySlide(index, activeSlide)
+        displayNextPreviousSlide(index, activePreviousSlide)
         displayActiveButton(activeButton, event)
     }
 })
@@ -88,6 +90,18 @@ buttons.addEventListener('click', (event)=>{
 function displaySlide (slideIndex, activeSlide){
     activeSlide.classList.remove('_active')
     slides[slideIndex].classList.add('_active')
+}
+
+function displayNextPreviousSlide (slideIndex, activePreviousSlide) {
+    activePreviousSlide.classList.remove('_active')
+
+    slideIndex++
+
+    if (slideIndex > feedbacks.length - 1) {
+        previousSlides[0].classList.add('_active')
+    } else {
+        previousSlides[slideIndex].classList.add('_active')
+    }
 }
 
 function displayActiveButton (activeButton, event) {
